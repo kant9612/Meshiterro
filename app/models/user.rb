6a1ddc0231490ccea973dbca9b4885c2a66750e0
kant9612 <kanstephentakayama@gmail.com>
2021-01-12 08:30:30 +0000

    [start] 3-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
         # （パスワードの正確性を検証）,（ユーザ登録や編集、削除）
         # （パスワードをリセット）,（ログイン情報を保存）,（emailのフォーマットなどのバリデーション）
   has_many :post_images, dependent: :destroy
+  # user<postimage で関連付ける
+  has_many :post_comments, dependent: :destroy
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'post_images#index'
   # deviseを使用する際にURLとしてusersを含むことを示している
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root to: 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
